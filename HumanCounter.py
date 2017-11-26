eea201a4b8f3adf8b41f170fe9983f95eb4aae11
Current Zeng <chrian.zeng@gmail.com>
2017-11-25 18:51:44 -0800

    Updated with python gui to display the results

--- HumanCounter.py
+++ HumanCounter.py
@@ -1,12 +1,45 @@
 from __future__ import print_function
 from imutils.object_detection import non_max_suppression
 import numpy as np
-import sys
 import imutils
 import cv2
+import threading
+from tkinter import *
+
+padding = 8
+total_num = 0
+
+
+def num_update(face_num, body_num):
+    root = Tk()
+    root.title("Detection Results")
+    root.resizable(0, 0)
+
+    face_lb = Label(root, text="Number of faces detected:")
+    num_face_lb = Label(root, text=str(face_num))
+    body_lb = Label(root, text="Number of bodies detected:")
+    num_body_lb = Label(root, text=str(body_num))
+    bt_close = Button(root, text="Close", command=root.destroy)
+    tot_lb = Label(root, text="The total number of human:")
+    tot_num_lb = Label(root, text=str(total_num))
+
+    num_face_lb.config(font=("",25))
+    num_body_lb.config(font=("",25))
+    tot_num_lb.config(font=("", 25))
+
+    face_lb.grid(row=0, column=0, padx=padding, pady=padding)
+    num_face_lb.grid(row=0, column=1, padx=padding, pady=padding)
+    body_lb.grid(row=1, column=0, padx=padding, pady=padding)
+    num_body_lb.grid(row=1, column=1, padx=padding, pady=padding)
+    tot_lb.grid(row=2, column=0, padx=padding, pady=padding)
+    tot_num_lb.grid(row=2, column=1, padx=padding, pady=padding)
+    bt_close.grid(row=3, column=2, padx=padding, pady=padding)
+
+    root.mainloop()
+    return
+
 
 face_cascade = cv2.CascadeClassifier('C:\OpenCV-3.3.0\opencv\sources\data\haarcascades\haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('C:\OpenCV-3.3.0\opencv\sources\data\haarcascades\haarcascade_eye.xml')
 
 # initialize the HOG descriptor/person detector
 hog = cv2.HOGDescriptor()
@@ -17,15 +50,9 @@ imagePath = sys.argv[1]
 
 image = cv2.imread(imagePath)
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(image, 1.1, 5)
+faces = face_cascade.detectMultiScale(image, 1.3, 5)
 for (x,y,w,h) in faces:
     cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = image[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,225),2)
 
 image = imutils.resize(image, width=min(400, image.shape[1]))
 orig = image.copy()
@@ -48,5 +75,12 @@ pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
 for (xA, yA, xB, yB) in pick:
     cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
 
+image = imutils.resize(image, width=800)
+
+threads = []
+t = threading.Thread(target=num_update, args=(len(faces), len(pick)))
+threads.append(t)
+t.start()
+
 cv2.imshow("Image", image)
-cv2.waitKey(0)
+c = cv2.waitKey(0)
